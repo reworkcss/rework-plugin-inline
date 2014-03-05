69e154286d20e3357a925f58842389eb497fa6fd
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-03-05 12:35:50 -0500

    Initial port of inline() code from rework

---
+++ test/rework-inline.js
@@ -0,0 +1,33 @@
+
+var rework = require('rework')
+  , fs = require('fs')
+  , assert = require('assert')
+  , read = fs.readFileSync;
+
+function fixture(name) {
+  return read('test/fixtures/' + name + '.css', 'utf8').trim();
+}
+
+describe('.inline(dir)', function () {
+  it('should inline images', function () {
+    rework(fixture('inline'))
+      .use(rework.inline('lib/', 'test/fixtures'))
+      .toString()
+      .should.equal(fixture('inline.out'));
+  })
+
+  it('should accept dirs in array', function () {
+    rework(fixture('inline'))
+      .use(rework.inline(['lib/', 'test/fixtures']))
+      .toString()
+      .should.equal(fixture('inline.out'));
+  })
+
+  it('should throw error on nonexistent file', function () {
+    (function () {
+      rework(fixture('inline'))
+        .use(rework.inline())
+        .toString()
+    }).should.throw(/dot.png/)
+  })
+})
